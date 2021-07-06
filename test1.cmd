1921aeac8855322d8bb5be57f93dbe6f660a16d2
Doug Ransom <doug.ransom@gmail.com>
2021-07-06 16:33:35 -0700

    removed test

--- test1.cmd
+++
@@ -1,3 +0,0 @@
-@echo on
-echo orders in dollars
-croesus_to_nbin_tradelist  src/nbin_golf/TestOrders.xlsx REPCODE 1
--- test3.bat
+++
@@ -1,2 +0,0 @@
-echo orders in shares, dividends cash
-croesus_to_nbin_tradelist  -d c --amount_type shares src/nbin_golf/TestOrders.xlsx REPCODE 3
